b24fd4ba5fdc46c2524d6509534228b24ad79f01
Chad Klem <github@cfk.me>
2022-10-01 07:12:12 -0700

    Better import statement

--- Games/RoadKill/components/sprite.py
+++ Games/RoadKill/components/sprite.py
@@ -1,13 +1,13 @@
-import pygame 
+from pygame.sprite import Sprite
 
-class SimpleSprite(pygame.sprite.Sprite):
+class SimpleSprite(Sprite):
 	def __init__(self,surf,pos,groups):
 		super().__init__(groups)
 		self.image = surf
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-self.rect.height / 2)
 
-class LongSprite(pygame.sprite.Sprite):
+class LongSprite(Sprite):
 	def __init__(self,surf,pos,groups):
 		super().__init__(groups)
 		self.image = surf
